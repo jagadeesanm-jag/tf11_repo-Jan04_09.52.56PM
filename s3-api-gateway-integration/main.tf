6a9cec2471898f92a1deb8259351c0394bf6743a
Jagadeesan Murugesan <jagadeesan.m@gmail.com>
2021-01-04 21:54:00 +0530

    Modified files:[main.tf]

--- s3-api-gateway-integration/main.tf
+++ s3-api-gateway-integration/main.tf
@@ -168,3 +168,4 @@ resource "aws_api_gateway_deployment" "S3APIDeployment" {
   rest_api_id = "${aws_api_gateway_rest_api.MyS3.id}"
   stage_name  = "MyS3"
 }
+# Modified [main.tf] on [2021-01-04 21:54:00.152690]
